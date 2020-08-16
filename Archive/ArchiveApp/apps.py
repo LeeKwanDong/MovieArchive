950f8a72b674d06e90c0dcce3e53f3e4b38619fd
LeeKwanDong <igwandong@gmail.com>
2020-08-16 17:35:46 +0900

    deleting older folders

--- Archive/ArchiveApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ArchiveappConfig(AppConfig):
-    name = 'ArchiveApp'
--- Archive/ArchiveApp/form.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Movies
-
-class Posters(forms.ModelForm):
-    class Meta:
-        model = Movies
-        fields = ["img"]  
